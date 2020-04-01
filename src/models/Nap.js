a522d0a1e52d8b19193e32e71b0450dc887e0e84
Ringo Rohe <ringo.rohe@gmail.com>
2020-04-01 21:44:01 +0200

    adds breakpoints scss, adds notes field to Nap

--- src/models/Nap.js
+++ src/models/Nap.js
@@ -1,26 +1,30 @@
 class Nap {
-    constructor(start, end, id) {
+    constructor(start, end, id, notes) {
         id ? this.id = id : this.id = null;
         start ? this.start = start : this.start = 0;
         end ? this.end = end : this.end = 0;
+        notes ? this.notes = notes : this.notes = null;
     }
 
     fromObject(object) {
         if (object.id) this.id = object.id;
         if (object.start) this.start = object.start;
         if (object.end) this.end = object.end;
+        if (object.notes) this.notes = object.notes;
     }
 
     fromFirebaseDoc(object) {
         this.id = object.id;
         this.start = object.data().start;
         this.end = object.data().end;
+        object.data().notes ? this.notes = object.data().notes : this.notes = null;
     }
 
     toObject() {
         return ({
             start: this.start,
-            end: this.end
+            end: this.end,
+            notes: this.notes ? this.notes : null
         });
     }
 }
