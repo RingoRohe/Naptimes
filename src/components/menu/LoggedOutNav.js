d25e2a26d6f340834459ab13253210df5da26077
Ringo Rohe <ringo.rohe@gmail.com>
2020-04-05 23:03:34 +0200

    updates info page

--- src/components/menu/LoggedOutNav.js
+++ src/components/menu/LoggedOutNav.js
@@ -5,7 +5,7 @@ const LoggedOutNav = () => {
     return (
         <ul className="main_menu">
             <li>
-                <Link className="menuPoint" to="/info">
+                <Link className="menuPoint" to="/onboarding">
                     <span className="icon fas fa-info fa-3x"></span>
                     <span className="text">Info</span>
                 </Link>
