1d387e6735038189bf205ab22ec456084f33dd60
Ringo Rohe <ringo.rohe@gmail.com>
2020-04-23 08:12:27 +0200

    changes Link to NavLink and gets rid of custom active-class Solution

--- src/components/menu/MenuPoint.js
+++ src/components/menu/MenuPoint.js
@@ -1,23 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const MenuPoint = props => {
-    let location = useLocation();
-
-    const getClasses = () => {
-        if (props.to === location.pathname) {
-            return 'menuPoint active';
-        } else {
-            return 'menuPoint';
-        }
-    }
 
     return (
-        <Link className={getClasses()} to={props.to}>
+        <NavLink exact className="menuPoint" to={props.to}>
             <span className={props.iconClasses}></span>
             <span className="text">{props.title}</span>
-        </Link>
+        </NavLink>
     );
 }
 
